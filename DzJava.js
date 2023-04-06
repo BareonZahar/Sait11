f00b6a97e1a3343dcf39ed6a51cf9556bbaf4097
BareonZahar <126097700+BareonZahar@users.noreply.github.com>
2023-04-06 19:30:41 +0300

    Update DzJava.js

--- DzJava.js
+++ DzJava.js
@@ -5,9 +5,9 @@ let    oke = document.getElementById('oke')
 
 
 function ret() {
-    let min=farn.value
-  let max=furk.value
-  chislo.innerHTML= Math.floor(Math.random() * (max - min + 1)+ min)
+    let min=+farn.value
+  let max=+furk.value
+  chislo.innerHTML= Math.floor(Math.random() * (max - min + 1))+ min
 }
 
 oke.onclick=ret
